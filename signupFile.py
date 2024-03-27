40e90964a78ea1e36e13855b434336d41ad082be
Natalie Russell <natprussell@gmail.com>
2024-03-27 16:35:06 -0500

    saving to reopen visual code

--- signupFile.py
+++ signupFile.py
@@ -1,15 +1,17 @@
 from tkinter import *
 from tkinter import ttk
+import sqlite3
+
 root = Tk()
 root.geometry("150x150")
-import sqlite3
+root.title("Sign Up")
+
 
 def signup():
     email = email_entry.get()
     password = password_entry.get()
     reenter_password = reenter_password_entry.get()
-    if "@" not in email:
-        status_label.config(text="Invalid email format. Please enter a valid email.")
+
     if password == reenter_password:
         conn = sqlite3.connect('SignUpInfo.db')
         c = conn.cursor()
@@ -18,16 +20,13 @@ def signup():
         conn.commit()
         conn.close()
 
-        email_entry.delete(0,END)
-        password_entry.delete(0, END)
-        reenter_password_entry.delete(0,END)
+        email_entry.delete(0, ttk.END)
+        password_entry.delete(0, ttk.END)
+        reenter_password_entry.delete(0, ttk.END)
 
         status_label.config(text="Sign up successful!")
     else:
         status_label.config(text="Passwords do not match. Please try again.")
-
-
-root.title("Sign Up")
 
 email_label = ttk.Label(root, text="Email:")
 email_label.pack()
